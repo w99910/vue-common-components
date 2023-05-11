cdd8748649f8b389cd5f26e3ebba819addc6cb27
Zaw Lin Tun <zawlintun@utycc.edu.mm>
2023-05-11 14:08:18 +0630

    add close button to popover

--- src/popover.vue
+++ src/popover.vue
@@ -1,8 +1,12 @@
 <template>
     <div :id="id"
          class="z-[1000] bg-[rgba(0,0,0,0.6)] pop-over w-screen flex items-start py-12 justify-center h-screen top-0 left-0 fixed overflow-x-hidden overflow-y-auto ">
-        <slot class="flex flex-col py-8 items-center justify-center">
-        </slot>
+        <div class="relative self-center">
+            <button @click="emits('click-outside')" v-html="closeIcon"
+                    class="z-10 w-5 h-5 text-gray-800 absolute top-2.5 right-2.5"></button>
+            <slot class="flex flex-col py-8 items-center justify-center">
+            </slot>
+        </div>
     </div>
 </template>
 
@@ -10,6 +14,7 @@
 // Emit an event called 'click-outside' when the user clicks outside the pop-over element
 import {onMounted, onUnmounted} from "vue";
 import {Random} from "js-utils";
+import {closeIcon} from "../assets/icons";
 
 const emits = defineEmits(['click-outside'])
 
