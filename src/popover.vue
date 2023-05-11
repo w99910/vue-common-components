8d2473f51bd688b1f3e4bb52d421f6682ee35ea9
Zaw Lin Tun <zawlintun@utycc.edu.mm>
2023-05-11 15:18:56 +0630

    add shouldCenter props to popover

--- src/popover.vue
+++ src/popover.vue
@@ -1,7 +1,7 @@
 <template>
     <div :id="id"
          class="z-[1000] bg-[rgba(0,0,0,0.6)] pop-over w-screen flex items-start py-12 justify-center h-screen top-0 left-0 fixed overflow-x-hidden overflow-y-auto ">
-        <div class="relative self-center">
+        <div class="relative" :class="{'self-center':shouldCenter}">
             <button @click="emits('click-outside')" v-html="closeIcon"
                     class="z-10 w-5 h-5 text-gray-800 absolute top-2.5 right-2.5"></button>
             <slot class="flex flex-col py-8 items-center justify-center">
@@ -17,6 +17,13 @@ import {Random} from "js-utils";
 import {closeIcon} from "../assets/icons";
 
 const emits = defineEmits(['click-outside'])
+
+const props = defineProps({
+    shouldCenter:{
+        type:Boolean,
+        default:true,
+    }
+})
 
 const id = Random.string(10)
 
